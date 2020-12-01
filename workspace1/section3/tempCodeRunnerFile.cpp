b0c37c2834976385d1650e84423c746679db0d19
kenji132 <ue52483@gmail.com>
2020-12-01 23:12:01 +0900

    構造体

--- workspace1/section3/tempCodeRunnerFile.cpp
+++ workspace1/section3/tempCodeRunnerFile.cpp
@@ -1,4 +1,24 @@
- for(int i = 0; i < N; i++){
-        cout << p[N].first << p[N].second;
-    }
-    cout << endl;
+second += diff_second;
+        if(diff_second >=60){
+            minute += 1;
+            second -= 60;
+        }else if(diff_second < 0){
+            minute -= 1;
+            second += 60;
+        }
+
+        minute += diff_minute;
+        if(diff_minute >= 60){
+            hour += 1;
+            minute -= 60;
+        }else if(diff_minute < 0){
+            hour -= 1;
+            minute += 60;
+        }
+
+        hour += diff_hour;
+        if(diff_hour >= 24){
+            hour -= 24;
+        }else if(diff_hour < 0){
+            hour += 24;
+        }
