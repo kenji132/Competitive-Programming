8868b7fa1a880dda8f7adef32b96247c1488cf92
kenji132 <ue52483@gmail.com>
2020-12-08 19:59:40 +0900

    ビット演算子の練習

--- workspace1/ABC過去問/ABC128/C.cpp
+++ workspace1/ABC過去問/ABC128/C.cpp
@@ -2,15 +2,40 @@
 using namespace std;
 int main(){
     int N,M;
-    
-    vector<vector<int>> data(M, vector<int>(M));
-    for(int i=1; i < (M+1); ++i){
-        for(int j=0; j < (M+1); ++j){
-            cin >> data[i][j];
+    cin >> N >> M;
+
+    vector<int> a(N);
+    for(int i = 0; i < M; i++){
+        int k;
+        cin >> k;
+        for(int j=0; j < k; j++){
+            int s;
+            cin >> s;
+            s--;
+            a[s] |= 1 << i;
         }
     }
-    vector<int> p;
-    for(int i=1; i < (M+1); ++i){
-        cin >> p[i];
+    int p=0;
+    for(int i=0; i < M; ++i){
+        int a;
+        cin >> a;
+        p |= a << i;
     }
+    int ans = 0;
+
+    //スイッチの入れ方の全探索
+    for(int s=0; s <(1 << N); s++){
+        int t= 0;
+        for(int i=0; i < N; ++i){
+            if(s >> i &1){
+                t^=a[i];//二回スイッチを押すともとに戻るのでxor演算子
+            }
+        }
+        if(t == p){
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
 }
