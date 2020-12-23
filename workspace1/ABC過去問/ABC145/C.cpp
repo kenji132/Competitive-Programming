aaa49e0f89a0b232c80602f08e1d96cf5310bd0d
kenji132 <ue52483@gmail.com>
2020-12-23 16:39:18 +0900

    ABC145 C

--- workspace1/ABC過去問/ABC145/C.cpp
+++ workspace1/ABC過去問/ABC145/C.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    double n;
     cin >> n;
     vector<double> x;
     vector<double> y;
@@ -12,5 +12,14 @@ int main(){
         y.push_back(b);
     }
 
-    for
+    double k = 1;
+    double ans = 0;
+    for(int i = 0; i < n; ++i){
+        k *= i+1;
+        for(int j = i+1; j < n; ++j){
+            ans += sqrt(pow(x[i] - x[j], 2) + pow(y[j] - y[i], 2));
+        }
+    }
+    ans *= (n-1)/(n*(n-1)/2);
+    cout << fixed << setprecision(10) << ans << endl;
 }
