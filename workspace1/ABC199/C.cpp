153baeccff05081fc2f97b81be13c69b1767cb6c
kenji132 <ue52483@gmail.com>
2021-04-27 21:47:53 +0900

    ABC199C

--- workspace1/ABC199/C.cpp
+++ workspace1/ABC199/C.cpp
@@ -4,32 +4,26 @@ using namespace std;
 int main(){
   int n;
   cin >> n;
-  string s,new_s;
+  string s;
   cin >> s;
   int q;
   cin >> q;
+  string s1 = s.substr(0, n);
+  string s2 = s.substr(n, n);
   for(int i = 0; i < q; i++){
     int t,a,b;
     cin >> t >> a >> b;
-    if(t == 1){
-      char sa,sb;
-      a--;
-      b--;
-      sa = s[a];
-      sb = s[b];
-      s[b] = sa;
-      s[a] = sb;
-    }else if(t == 2){
-      new_s = s;
-      string s1 = s.substr(0, n);
-      string s2 = s.substr(n, n);
-
-      new_s = s2+s1;
-      // for(int i = 0; i < 2*n; i++){
-      //   new_s[(i+n)%(2*n)] = s[i];
-      // }
-      s = new_s;
+    a--;
+    b--;
+    if(t == 2){
+      swap(s1,s2);
+    }else if(a < n && b < n){
+      swap(s1[a],s1[b]);
+    }else if(a < n){
+      swap(s1[a], s2[b-n]);
+    }else{
+      swap(s2[a-n], s2[b-n]);
     }
   }
-  cout << s << endl;
+  cout << s1 + s2 << endl;
 }
